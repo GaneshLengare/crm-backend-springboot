808a35181a7f80e95da37202bde777309f7847cb
GaneshLengare <148607444+GaneshLengare@users.noreply.github.com>
2026-02-12 02:23:11 +0530

    Spring Initializer Project Structure

---
+++ src/test/java/com/ganesh/crm/CrmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganesh.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
